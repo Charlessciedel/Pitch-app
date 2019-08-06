1830282f0d345c4c72e5009cd7f3a093d0cda92f
George Charles <charlesschiedel@gmail.com>
2019-08-06 12:56:23 +0300

    Initialized app

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export SECRET_KEY='123'
+# export SECRET_KEY='123'
 
-export MAIL_USERNAME='charlesschiedel@gmail.com'
-export MAIL_PASSWORD='qwert321'
-python3 manage.py server
+# export MAIL_USERNAME='charlesschiedel@gmail.com'
+# export MAIL_PASSWORD='qwert321'
+# python3 manage.py server
