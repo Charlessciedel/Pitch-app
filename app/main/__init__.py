70adc5b4cb5e34f2870076504c48661e71d1f8aa
George Charles <charlesschiedel@gmail.com>
2019-08-06 13:01:01 +0300

    I have made a config.py file

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,6 @@
-# from flask import Blueprint
+from flask import Blueprint
 
-# main = Blueprint( 'main' , __name__ )
+main = Blueprint( 'main' , __name__ )
 
-# from . import views, errors
+from . import views, errors
 
